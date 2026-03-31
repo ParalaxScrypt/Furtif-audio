0b0dce0e0b3155f723c6f5be4d9d506c2eb64347
ParalaxScrypt <lauziard.mathieu@hotmail.fr>
2026-03-31 17:38:17 +0200

    Add files via upload

---
+++ bin/flash.bat
@@ -0,0 +1,30 @@
+@echo off
+REM ============================================
+REM  Furtif-Audio - Flash ESP32 (tous les fichiers)
+REM ============================================
+
+set COM_PORT=COM3
+set BAUD=921600
+
+REM ----------------------------
+REM Flash bootloader
+REM ----------------------------
+echo Flashing bootloader...
+python -m esptool --chip esp32 --port %COM_PORT% --baud %BAUD% write-flash 0x1000 furtif.ino.bootloader.bin
+
+REM ----------------------------
+REM Flash partitions
+REM ----------------------------
+echo Flashing partitions...
+python -m esptool --chip esp32 --port %COM_PORT% --baud %BAUD% write-flash 0x8000 furtif.ino.partitions.bin
+
+REM ----------------------------
+REM Flash firmware
+REM ----------------------------
+echo Flashing firmware...
+python -m esptool --chip esp32 --port %COM_PORT% --baud %BAUD% write-flash 0x10000 furtif.ino.bin
+
+REM ----------------------------
+REM Fin
+REM ----------------------------
+echo All flashing steps complete! Unplug and replug your ESP32.
